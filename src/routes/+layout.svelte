a4b8709a169cc9e94067d3d2dbddbdab6679d429
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-06-14 23:29:20 +0000

    layout: add footer and navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+    import Navbar from "../components/navbar.svelte";
+    import Footer from "../components/footer.svelte";
     import "../app.css";
 
     let { children } = $props();
 </script>
 
-{@render children()}
+<div class="bg-secondary-400 min-h-screen flex flex-col">
+    <Navbar />
+    <div class="flex-grow">
+        {@render children()}
+    </div>
+    <Footer />
+</div>
