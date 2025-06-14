f5ce731a6e563dc260ea65288997fa00b2a4bb61
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-06-14 23:28:59 +0000

    components: add footer and navbar

---
+++ src/components/footer.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {
+        Footer,
+        FooterCopyright,
+        FooterLinkGroup,
+        FooterBrand,
+        FooterLink,
+    } from "flowbite-svelte";
+</script>
+
+<Footer footerType="logo">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <FooterBrand
+            href="/"
+            src="/hampter.jpg"
+            alt="Hamster Logo"
+            name="Home"
+        />
+        <FooterLinkGroup
+            class="mb-6 flex flex-wrap items-center text-sm text-gray-500 sm:mb-0 dark:text-gray-400"
+        >
+            <FooterLink href="/about">About</FooterLink>
+        </FooterLinkGroup>
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
+    <FooterCopyright href="/" by="Mateusz Domalewski" />
+</Footer>
